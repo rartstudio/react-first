cc02e0050499d7972b7add98ede603c742cd8dc9
rudiyanto <rudiyanto@dwebsite.net>
2021-05-23 11:06:08 +0700

    starter project with bs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <button className="mx-4 my-2 py-2 px-4 alert alert-primary">Hello From Bootstrap</button>
+      </div>
     </div>
   );
 }
